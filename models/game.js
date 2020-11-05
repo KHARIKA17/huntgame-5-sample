eaeb04167ad8a3ecdba12f9580e763cf5e242689
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2020-11-04 20:53:55 -0600

    modified app

---
+++ models/game.js
@@ -0,0 +1,21 @@
+/**
+ *  Team model
+ *  Describes the characteristics of each attribute in a team resource.
+ *
+ * @author Blake Bennett <s532542@nwmissouri.edu>
+ *
+ * For more information about defining sequelize models, see
+ * https://sequelize.org/v5/manual/data-types.html
+ *
+ */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
+
+module.exports = (sequelize, DataTypes) => {
+  sequelize.define('Game', {
+    // sqlite creates a rowid attribute automatically
+    name: { type: DataTypes.STRING(30) },
+    playerCount: { type: DataTypes.INTEGER },
+    isCardGame: { type: DataTypes.BOOLEAN },
+  });
+};
